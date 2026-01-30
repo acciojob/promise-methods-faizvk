dcdf4178bada02d6f13a9ec414fdae4e0b86fa03
Faiz Zubair <faizvk14@gmail.com>
2026-01-30 13:34:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,94 @@
+Promise.myRace = function (promises) {
+  return new Promise((resolve, reject) => {
+    for (let p of promises) {
+      Promise.resolve(p).then(resolve).catch(reject);
+    }
+  });
+};
+
+Promise.myAny = function (promises) {
+  return new Promise((resolve, reject) => {
+    let rejectCount = 0;
+
+    for (let p of promises) {
+      Promise.resolve(p)
+        .then(resolve)
+        .catch(() => {
+          rejectCount++;
+          if (rejectCount === promises.length) {
+            reject("all promises rejected");
+          }
+        });
+    }
+  });
+};
+
+Promise.myAll = function (promises) {
+  return new Promise((resolve, reject) => {
+    const results = new Array(promises.length);
+    let fulfilledCount = 0;
+
+    promises.forEach((p, index) => {
+      Promise.resolve(p)
+        .then(value => {
+          results[index] = value;
+          fulfilledCount++;
+
+          if (fulfilledCount === promises.length) {
+            resolve(results);
+          }
+        })
+        .catch(err => {
+          reject(err); // immediate rejection
+        });
+    });
+  });
+};
+
+Promise.myAllSettled = function (promises) {
+  return new Promise(resolve => {
+    const results = new Array(promises.length);
+    let settledCount = 0;
+
+    promises.forEach((p, index) => {
+      Promise.resolve(p)
+        .then(value => {
+          results[index] = {
+            status: "fulfilled",
+            value: value
+          };
+        })
+        .catch(error => {
+          results[index] = {
+            status: "rejected",
+            error: error
+          };
+        })
+        .finally(() => {
+          settledCount++;
+          if (settledCount === promises.length) {
+            resolve(results);
+          }
+        });
+    });
+  });
+};
+
+// for your export requirement
 function myRace(promises) {
-  // implementation
+  return Promise.myRace(promises);
 }
 
 function myAny(promises) {
-  // implementation
+  return Promise.myAny(promises);
 }
 
 function myAll(promises) {
-  // implementation
+  return Promise.myAll(promises);
 }
 
 function myAllSettled(promises) {
-  // implementation
+  return Promise.myAllSettled(promises);
 }
 
 module.exports = {
